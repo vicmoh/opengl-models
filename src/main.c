d6d9b5779c78109523631de50e92051e2faae17b
Vicky Mohammad <vimoh1213@gmail.com>
2020-03-29 18:01:25 -0400

    clean the code in main

--- src/main.c
+++ src/main.c
@@ -47,6 +47,7 @@ static Point _cameraPos = {.x = 0, .y = 0, .z = 0};
  */
 void drawFace(int index) {
   MEM_TRACK
+  glRotatef(_rotate, 0.0, 1.0, 0.0);
   Splitter *faceSplit = Model_parsedData->faceList->at[index];
   if (isStringEqual(faceSplit->at[0], "3")) glBegin(GL_TRIANGLES);
   if (isStringEqual(faceSplit->at[0], "4")) glBegin(GL_QUADS);
@@ -76,6 +77,11 @@ void drawFace(int index) {
  * Draw Based on parsed data.
  */
 void drawModel() { for_in(next, Model_parsedData->faceList) drawFace(next); }
+
+void update() {
+  _rotate += 1.0;
+  drawModel();
+}
 
 /* -------------------------------------------------------------------------- */
 /*                              Open GL Functions                             */
@@ -111,22 +117,6 @@ int lightMoving = 0, lightStartX, lightStartY;
 enum { MISSING, EXTENSION, ONE_DOT_ONE };
 int polygonOffsetVersion;
 
-static GLdouble bodyWidth = 3.0;
-/* *INDENT-OFF* */
-static GLfloat body[][2] = {
-    {0, 3},     {1, 1},   {5, 1},   {8, 4},     {10, 4},  {11, 5},
-    {11, 11.5}, {13, 12}, {13, 13}, {10, 13.5}, {13, 14}, {13, 15},
-    {11, 16},   {8, 16},  {7, 15},  {7, 13},    {8, 12},  {7, 11},
-    {6, 6},     {4, 3},   {3, 2},   {1, 2}};
-static GLfloat arm[][2] = {{8, 10},  {9, 9},     {10, 9},    {13, 8},
-                           {14, 9},  {16, 9},    {15, 9.5},  {16, 10},
-                           {15, 10}, {15.5, 11}, {14.5, 10}, {14, 11},
-                           {14, 10}, {13, 9},    {11, 11},   {9, 11}};
-static GLfloat leg[][2] = {{8, 6},   {8, 4},  {9, 3},  {9, 2},  {8, 1},
-                           {8, 0.5}, {9, 0},  {12, 0}, {10, 1}, {10, 2},
-                           {12, 4},  {11, 6}, {10, 7}, {9, 7}};
-static GLfloat eye[][2] = {{8.75, 15}, {9, 14.7},    {9.6, 14.7},
-                           {10.1, 15}, {9.6, 15.25}, {9, 15.25}};
 static GLfloat lightPosition[4];
 static GLfloat lightColor[] = {0.8, 1.0, 0.8, 1.0}; /* green-tinted */
 static GLfloat skinColor[] = {0.1, 1.0, 0.1, 1.0},
@@ -160,6 +150,22 @@ typedef enum {
   EYE_WHOLE
 } displayLists;
 
+enum {
+  M_NONE,
+  M_MOTION,
+  M_LIGHT,
+  M_TEXTURE,
+  M_SHADOWS,
+  M_REFLECTION,
+  M_DINOSAUR,
+  M_STENCIL_REFLECTION,
+  M_STENCIL_SHADOW,
+  M_OFFSET_SHADOW,
+  M_POSITIONAL,
+  M_DIRECTIONAL,
+  M_PERFORMANCE
+};
+
 /* Create a matrix that will project the desired shadow. */
 void shadowMatrix(GLfloat shadowMat[4][4], GLfloat groundplane[4],
                   GLfloat lightpos[4]) {
@@ -174,17 +180,17 @@ void shadowMatrix(GLfloat shadowMat[4][4], GLfloat groundplane[4],
   shadowMat[2][0] = 0.f - lightpos[X] * groundplane[Z];
   shadowMat[3][0] = 0.f - lightpos[X] * groundplane[W];
 
-  shadowMat[X][1] = 0.f - lightpos[Y] * groundplane[X];
+  shadowMat[0][1] = 0.f - lightpos[Y] * groundplane[X];
   shadowMat[1][1] = dot - lightpos[Y] * groundplane[Y];
   shadowMat[2][1] = 0.f - lightpos[Y] * groundplane[Z];
   shadowMat[3][1] = 0.f - lightpos[Y] * groundplane[W];
 
-  shadowMat[X][2] = 0.f - lightpos[Z] * groundplane[X];
+  shadowMat[0][2] = 0.f - lightpos[Z] * groundplane[X];
   shadowMat[1][2] = 0.f - lightpos[Z] * groundplane[Y];
   shadowMat[2][2] = dot - lightpos[Z] * groundplane[Z];
   shadowMat[3][2] = 0.f - lightpos[Z] * groundplane[W];
 
-  shadowMat[X][3] = 0.f - lightpos[W] * groundplane[X];
+  shadowMat[0][3] = 0.f - lightpos[W] * groundplane[X];
   shadowMat[1][3] = 0.f - lightpos[W] * groundplane[Y];
   shadowMat[2][3] = 0.f - lightpos[W] * groundplane[Z];
   shadowMat[3][3] = dot - lightpos[W] * groundplane[W];
@@ -211,105 +217,9 @@ void findPlane(GLfloat plane[4], GLfloat v0[3], GLfloat v1[3], GLfloat v2[3]) {
   plane[D] = -(plane[A] * v0[X] + plane[B] * v0[Y] + plane[C] * v0[Z]);
 }
 
-void extrudeSolidFromPolygon(GLfloat data[][2], unsigned int dataSize,
-                             GLdouble thickness, GLuint side, GLuint edge,
-                             GLuint whole) {
-  static GLUtriangulatorObj *tobj = NULL;
-  GLdouble vertex[3], dx, dy, len;
-  int i;
-  int count = dataSize / (2 * sizeof(GLfloat));
-
-  if (tobj == NULL) {
-    tobj = gluNewTess(); /* create and initialize a GLU
-                            polygon * * tesselation object */
-    gluTessCallback(tobj, GLU_BEGIN, glBegin);
-    gluTessCallback(tobj, GLU_VERTEX, glVertex2fv); /* semi-tricky */
-    gluTessCallback(tobj, GLU_END, glEnd);
-  }
-  glNewList(side, GL_COMPILE);
-  glShadeModel(GL_SMOOTH); /* smooth minimizes seeing
-                              tessellation */
-  gluBeginPolygon(tobj);
-  for (i = 0; i < count; i++) {
-    vertex[0] = data[i][0];
-    vertex[1] = data[i][1];
-    vertex[2] = 0;
-    gluTessVertex(tobj, vertex, data[i]);
-  }
-  gluEndPolygon(tobj);
-  glEndList();
-  glNewList(edge, GL_COMPILE);
-  glShadeModel(GL_FLAT); /* flat shade keeps angular hands
-                            from being "smoothed" */
-  glBegin(GL_QUAD_STRIP);
-  for (i = 0; i <= count; i++) {
-    /* mod function handles closing the edge */
-    glVertex3f(data[i % count][0], data[i % count][1], 0.0);
-    glVertex3f(data[i % count][0], data[i % count][1], thickness);
-    /* Calculate a unit normal by dividing by Euclidean
-       distance. We * could be lazy and use
-       glEnable(GL_NORMALIZE) so we could pass in * arbitrary
-       normals for a very slight performance hit. */
-    dx = data[(i + 1) % count][1] - data[i % count][1];
-    dy = data[i % count][0] - data[(i + 1) % count][0];
-    len = sqrt(dx * dx + dy * dy);
-    glNormal3f(dx / len, dy / len, 0.0);
-  }
-  glEnd();
-  glEndList();
-  glNewList(whole, GL_COMPILE);
-  glFrontFace(GL_CW);
-  glCallList(edge);
-  glNormal3f(0.0, 0.0, -1.0); /* constant normal for side */
-  glCallList(side);
-  glPushMatrix();
-  glTranslatef(0.0, 0.0, thickness);
-  glFrontFace(GL_CCW);
-  glNormal3f(0.0, 0.0, 1.0); /* opposite normal for other side */
-  glCallList(side);
-  glPopMatrix();
-  glEndList();
-}
-
-static void makeDinosaur(void) {
-  extrudeSolidFromPolygon(body, sizeof(body), bodyWidth, BODY_SIDE, BODY_EDGE,
-                          BODY_WHOLE);
-  extrudeSolidFromPolygon(arm, sizeof(arm), bodyWidth / 4, ARM_SIDE, ARM_EDGE,
-                          ARM_WHOLE);
-  extrudeSolidFromPolygon(leg, sizeof(leg), bodyWidth / 2, LEG_SIDE, LEG_EDGE,
-                          LEG_WHOLE);
-  extrudeSolidFromPolygon(eye, sizeof(eye), bodyWidth + 0.2, EYE_SIDE, EYE_EDGE,
-                          EYE_WHOLE);
-}
-
-static void drawDinosaur(void) {
-  glPushMatrix();
-  /* Translate the dinosaur to be at (0,8,0). */
-  glTranslatef(-8, 0, -bodyWidth / 2);
-  glTranslatef(0.0, jump, 0.0);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, skinColor);
-  glCallList(BODY_WHOLE);
-  glTranslatef(0.0, 0.0, bodyWidth);
-  glCallList(ARM_WHOLE);
-  glCallList(LEG_WHOLE);
-  glTranslatef(0.0, 0.0, -bodyWidth - bodyWidth / 4);
-  glCallList(ARM_WHOLE);
-  glTranslatef(0.0, 0.0, -bodyWidth / 4);
-  glCallList(LEG_WHOLE);
-  glTranslatef(0.0, 0.0, bodyWidth / 2 - 0.1);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, eyeColor);
-  glCallList(EYE_WHOLE);
-  glPopMatrix();
-}
-
 /* Draw a floor (possibly textured). */
 static void drawFloor(void) {
   glDisable(GL_LIGHTING);
-
-  if (useTexture) {
-    glEnable(GL_TEXTURE_2D);
-  }
-
   glBegin(GL_QUADS);
   glTexCoord2f(0.0, 0.0);
   glVertex3fv(floorVertices[0]);
@@ -320,11 +230,6 @@ static void drawFloor(void) {
   glTexCoord2f(16.0, 0.0);
   glVertex3fv(floorVertices[3]);
   glEnd();
-
-  if (useTexture) {
-    glDisable(GL_TEXTURE_2D);
-  }
-
   glEnable(GL_LIGHTING);
 }
 
@@ -339,12 +244,13 @@ static void redraw(void) {
   // /* Reposition the light source. */
   // // lightPosition[0] = 12 * cos(lightAngle);
   lightPosition[1] = lightHeight;
-  lightPosition[2] = 12 * sin(lightAngle);
-  if (directionalLight) {
-    lightPosition[3] = 0.0;
-  } else {
-    lightPosition[3] = 1.0;
-  }
+  /*
+lightPosition[2] = 12 * sin(lightAngle);
+if (directionalLight) {
+lightPosition[3] = 0.0;
+} else {
+lightPosition[3] = 1.0;
+}*/
 
   shadowMatrix(floorShadow, floorPlane, lightPosition);
 
@@ -363,7 +269,7 @@ static void redraw(void) {
 
   /* Draw "bottom" of floor in blue. */
   glFrontFace(GL_CW); /* Switch face orientation. */
-  glColor4f(0.1, 0.1, 0.7, 1.0);
+  glColor4f(0.1, 0.1, 0.1, 1.0);
   drawFloor();
   glFrontFace(GL_CCW);
 
@@ -499,22 +405,6 @@ static void idle(void) {
   glutPostRedisplay();
 }
 
-enum {
-  M_NONE,
-  M_MOTION,
-  M_LIGHT,
-  M_TEXTURE,
-  M_SHADOWS,
-  M_REFLECTION,
-  M_DINOSAUR,
-  M_STENCIL_REFLECTION,
-  M_STENCIL_SHADOW,
-  M_OFFSET_SHADOW,
-  M_POSITIONAL,
-  M_DIRECTIONAL,
-  M_PERFORMANCE
-};
-
 static void controlLights(int value) {
   switch (value) {
     case M_NONE:
@@ -610,95 +500,36 @@ static int supportsOneDotOne(void) {
 int main(int argc, char **argv) {
   Model_parseModel(argc, argv);
 
+  // Init the window.
   glutInit(&argc, argv);
-
-  for (int i = 1; i < argc; i++) {
-    if (!strcmp("-linear", argv[i])) {
-      linearFiltering = 1;
-    } else if (!strcmp("-mipmap", argv[i])) {
-      useMipmaps = 1;
-    } else if (!strcmp("-ext", argv[i])) {
-      forceExtension = 1;
-    }
-  }
-
   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_STENCIL |
                       GLUT_MULTISAMPLE);
+  glutCreateWindow(argv[1]);
 
-#if 0
-  /* In GLUT 4.0, you'll be able to do this an be sure to
-     get 2 bits of stencil if the machine has it for you. */
-  glutInitDisplayString("samples stencil>=2 rgb double depth");
-#endif
-
-  glutCreateWindow("Shadowy Leapin' Lizards");
-
-  if (glutGet(GLUT_WINDOW_STENCIL_SIZE) <= 1) {
-    printf("dinoshade: Sorry, I need at least 2 bits of stencil.\n");
-    exit(1);
-  }
-
-  /* Register GLUT callbacks. */
+  // Init the open gl callbacks.
   glutDisplayFunc(redraw);
   glutMouseFunc(mouse);
   glutMotionFunc(motion);
   glutVisibilityFunc(visible);
   glutKeyboardFunc(key);
+  glutIdleFunc(update);
   glutSpecialFunc(special);
 
-  glutCreateMenu(controlLights);
-
-  glutAddMenuEntry("Toggle motion", M_MOTION);
-  glutAddMenuEntry("-----------------------", M_NONE);
-  glutAddMenuEntry("Toggle light", M_LIGHT);
-  glutAddMenuEntry("Toggle texture", M_TEXTURE);
-  glutAddMenuEntry("Toggle shadows", M_SHADOWS);
-  glutAddMenuEntry("Toggle reflection", M_REFLECTION);
-  glutAddMenuEntry("Toggle dinosaur", M_DINOSAUR);
-  glutAddMenuEntry("-----------------------", M_NONE);
-  glutAddMenuEntry("Toggle reflection stenciling", M_STENCIL_REFLECTION);
-  glutAddMenuEntry("Toggle shadow stenciling", M_STENCIL_SHADOW);
-  glutAddMenuEntry("Toggle shadow offset", M_OFFSET_SHADOW);
-  glutAddMenuEntry("----------------------", M_NONE);
-  glutAddMenuEntry("Positional light", M_POSITIONAL);
-  glutAddMenuEntry("Directional light", M_DIRECTIONAL);
-  glutAddMenuEntry("-----------------------", M_NONE);
-  glutAddMenuEntry("Toggle performance", M_PERFORMANCE);
-  glutAttachMenu(GLUT_RIGHT_BUTTON);
-  makeDinosaur();
-
-#ifdef GL_VERSION_1_1
-  if (supportsOneDotOne() && !forceExtension) {
-    polygonOffsetVersion = ONE_DOT_ONE;
-    glPolygonOffset(-2.0, -1.0);
-  } else
-#endif
-  {
-#ifdef GL_EXT_polygon_offset
-    /* check for the polygon offset extension */
-    if (glutExtensionSupported("GL_EXT_polygon_offset")) {
-      polygonOffsetVersion = EXTENSION;
-      glPolygonOffsetEXT(-0.1, -0.002);
-    } else
-#endif
-      polygonOffsetVersion = MISSING;
-    printf("\ndinoshine: Missing polygon offset.\n");
-    printf("           Expect shadow depth aliasing artifacts.\n\n");
-  }
-
+  // Init the modes and enable.
+  glPolygonOffset(-2.0, -1.0);
   glEnable(GL_CULL_FACE);
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_TEXTURE_2D);
   glLineWidth(3.0);
-
   glMatrixMode(GL_PROJECTION);
-  gluPerspective(/* field of view in degree */ 40.0,
-                 /* aspect ratio */ 1.0,
-                 /* Z near */ 20.0, /* Z far */ 100.0);
+  gluPerspective(40.0,    // Field of view in degree
+                 1.0,     // Asplect ratio
+                 20.0,    // The z near
+                 100.0);  // The z far
   glMatrixMode(GL_MODELVIEW);
-  gluLookAt(0.0, 8.0, 60.0, /* eye is at (0,0,30) */
-            0.0, 8.0, 0.0,  /* center is at (0,0,0) */
-            0.0, 1.0, 0.);  /* up is in postivie Y direction */
+  gluLookAt(0.0, 8.0, 60.0,  // eye is at (0,0,30)
+            0.0, 8.0, 0.0,   // center is at (0,0,0)
+            0.0, 1.0, 0.);   // up is in positive Y direction
 
   glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, 1);
   glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
